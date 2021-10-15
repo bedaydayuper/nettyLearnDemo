41acf9fd30659db93314dab9c99bae023a542132
zhangpeng <zhangpeng@ushareit.com>
2021-10-15 08:23:18 +0800

    hello world

---
+++ src/main/java/src/Main.java
@@ -0,0 +1,12 @@
+package src;
+
+/**
+ * @author zhangpeng
+ * @Date 2021/10/15
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("HELLO WORLD");
+    }
+}
